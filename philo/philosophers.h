63f4505dc6d30c68e98b89060a42be4bcf48b776
Lucas Bordonal <lbordonal@gmail.com>
2023-07-31 13:09:50 +0100

    31/07/23 - 13:09

--- philo/philosophers.h
+++ philo/philosophers.h
@@ -20,6 +20,42 @@
 # include <sys/time.h>
 # include <limits.h>
 
+typedef struct s_input
+{
+	int	num_philo;
+	int	time_to_die;
+	int	time_to_eat;
+	int	time_to_sleep;
+	int	num_of_times_eat;
+}		t_input;
+
+typedef struct s_fork
+{
+	int	left;
+	int	right;
+}		t_fork;
+
+typedef struct s_philo
+{
+	int			id;
+	int			num_of_times_ate;
+	long long	time_to_die;
+	t_fork		fork;
+	pthread_t	thread;
+}				t_philo;
+
+typedef struct s_main
+{
+	int				n_thread;
+	int				philo_dead;
+	long long		t0;
+	t_input			input;
+	t_philo			*philo;
+	pthread_t		orchestrator;
+	pthread_mutex_t	*forks;
+	phtread_mutex_t	write;
+}					t_main;
+
 /* philo.c: */
 
 /* libft_functions.c: */
